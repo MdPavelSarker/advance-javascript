a2021b59906e6629bd8a76aac5de73d079018794
MdPavelSarker <pavelsarkergithub@gmail.com>
2022-05-17 23:59:15 +0600

    try

---
+++ null-vs-undefined1.js
@@ -0,0 +1,17 @@
+let pakhi;
+function add(num1 , num2){
+    console.log(num1 + num2);
+    return add;
+}
+
+function add (num1 , num2){
+    console.log(num2);
+}
+
+const boys = {name:"smart dude", phon:456822};
+console.log(boys.address);     undefined
+
+let fun = undefined;
+console.log(fun);
+
+null
